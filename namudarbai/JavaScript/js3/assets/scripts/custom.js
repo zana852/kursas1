e094c2b21f2dd772f131a6ae73e167281d4e9572
Zana <zanabatajeva@gmail.com>
2019-08-04 10:52:51 +0200

    Atliktas namų darbas JS3

---
+++ namudarbai/JavaScript/js3/assets/scripts/custom.js
@@ -0,0 +1,17 @@
+//alert("pavyko"); // trying message
+
+
+function twoDivide(x, y){
+  var x=prompt("Input dividor x");
+  var y=prompt("Input divident y");
+  let divide = y/x;
+   alert("Result of dividion y/x is "+divide);
+  return divide;
+  }
+
+let divide = twoDivide();// trying test
+
+
+
+
+
