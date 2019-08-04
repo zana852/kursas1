dd5adf47e416b7ee1c0f25e3dca4966c666f6281
Zana <zanabatajeva@gmail.com>
2019-08-04 22:27:53 +0200

    Atliktas namų darbas JS4

---
+++ namudarbai/JavaScript/js4/assets/scripts/custom.js
@@ -0,0 +1,13 @@
+//alert ("pavyko!"); //message for checking JS
+
+function yourAge(){
+let x=prompt("Input age, please ");
+if (x<18){
+  alert("Sorry. This WEB site has still clothed for You.");
+  }else{
+  alert("OK! You are wellcome on this WEB site!");
+ }
+return x;
+}
+
+let x=yourAge();
