81cd40a6d7ccc7476a661fbe4f751d755f1d5e5d
Zana <zanabatajeva@gmail.com>
2019-08-06 17:14:19 +0200

    Atliktas namų darbas JS7

---
+++ namudarbai/JavaScript/js7/assets/scripts/custom.js
@@ -0,0 +1,10 @@
+//alert ("all well!"); //message for checking JS
+
+var text="";
+var mname="Dhana";
+var i;
+let k=prompt("Input one whole and positive number and search for result on consol, please");
+for (i=0; i<k; i++){
+  text+=mname+" ";
+}
+console.log(text);
