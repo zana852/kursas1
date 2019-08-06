6ca2af0fecb67d39227310d8a69f083fd0f1847a
Zana <zanabatajeva@gmail.com>
2019-08-06 16:14:01 +0200

    Atliktas namų darbas JS6

---
+++ namudarbai/JavaScript/js6/assets/scripts/custom.js
@@ -0,0 +1,8 @@
+//alert ("all well!"); //message for checking JS
+
+function yourNameSname(){
+let x=prompt("Input Your first name, please ");
+let y=prompt("Input Your second name, please ")
+  alert(x+" "+y+" "+"has just connected.");
+}
+let x,y=yourNameSname();
