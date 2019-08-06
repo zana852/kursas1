fcf038668cba206c83a51c1376cdf64629a337c2
Zana <zanabatajeva@gmail.com>
2019-08-06 15:55:12 +0200

    Kitų failų Commitas

--- namudarbai/JavaScript/js3/assets/scripts/custom.js
+++ namudarbai/JavaScript/js3/assets/scripts/custom.js
@@ -1,11 +1,11 @@
 //alert("pavyko"); // trying message
 
 
-function twoDivide(x, y){
-  var x=prompt("Input dividor x");
-  var y=prompt("Input divident y");
+function twoDivide(){
+ var x=prompt("Input dividor x");
+ var y=prompt("Input divident y");
   let divide = y/x;
-   alert("Result of dividion y/x is "+divide);
+  alert("Result of dividion y/x is "+divide);
   return divide;
   }
 
