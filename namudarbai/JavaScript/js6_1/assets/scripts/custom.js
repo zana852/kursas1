32da351a8e523c3933819ebbeaa721f3c69aff2e
Zana <zanabatajeva@gmail.com>
2019-08-07 17:08:23 +0200

    Koreguotas JS6

---
+++ namudarbai/JavaScript/js6_1/assets/scripts/custom.js
@@ -0,0 +1,9 @@
+//alert ("all well!"); //message for checking JS
+var x,y,z;
+function connectionWithNameSecondname(x,y){
+  var x=prompt("Input Your first name, please ");
+  var y=prompt("Input Your second name, please ");
+  var z=x+" "+y+"has just connected";
+  console.log(z);
+}
+connectionWithNameSecondname(x,y);
