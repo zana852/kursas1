171d780d19a6a827291927a54cbbdc9cae2435f9
Zana <zanabatajeva@gmail.com>
2019-08-08 01:34:43 +0200

    Koreguotas namų darbas JS5 su įvedimų ir išvedimu į .html

---
+++ namudarbai/JavaScript/js5_1/assets/scripts/custom.js
@@ -0,0 +1,10 @@
+//alert ("all good!"); //message for checking JS
+var x;
+function yourAge(x){
+  if (x>=18) {
+	document.write(" TRUE ");
+  }else{
+    document.write(" FALSE ");
+  }
+return (x);
+}
