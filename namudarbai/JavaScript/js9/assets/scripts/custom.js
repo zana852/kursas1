ffc3f101cfddd51e71700ec8d86b97b12fdef6e8
Zana <zanabatajeva@gmail.com>
2019-08-07 13:46:38 +0200

    Atliktas namų darbas JS9

---
+++ namudarbai/JavaScript/js9/assets/scripts/custom.js
@@ -0,0 +1,12 @@
+//alert ("all well!"); //message for checking  JS
+var text="";
+function arrayForZigmantas(index){
+  let list = ["Jonas Jonaitis", "Onutė Pakalnutė", "Juozas Juozaitis", "Marytė Rytė", "Antanas Antanaitis"];
+  for (i=0; i<list.length; i++){
+  text=[i+1]+["  "]+list[i];
+  console.log(text);
+  '/n';
+   }
+  return text;
+}
+arrayForZigmantas();
