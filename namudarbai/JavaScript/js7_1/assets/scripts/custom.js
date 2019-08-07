01300ffd240202b3d7b5dd9dfe344ed2df3074ae
Zana <zanabatajeva@gmail.com>
2019-08-07 20:20:52 +0200

    Koreguotas namų darbas JS7 -dabar su funkcija

---
+++ namudarbai/JavaScript/js7_1/assets/scripts/custom.js
@@ -0,0 +1,12 @@
+//alert ("all well!"); //message for checking JS
+
+var text="";
+var mname="Dhana";
+var i;
+function printMyNameKTimes(k){
+  for (i=0; i<k; i++){
+    text+=mname+" ";
+  }
+console.log(text);
+}
+printMyNameKTimes(5);
